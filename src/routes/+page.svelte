0c8362be620242690952dd7f32c2ad0b99b5801e
gregor <gregor.bucar99@gmail.com>
2023-01-25 01:57:25 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import restaurants from "./restaurants.json";
+  import Restaurant from "./Restaurant.svelte";
+  let res = restaurants;
+
+  let cheapFirst = true;
+  let city = "LJUBLJANA";
+  let justOpen = true;
+
+  $: res = res.sort((a,b)=> cheapFirst ? a.cena-b.cena : b.cena - a.cena);
+  $: res = res.filter(a => a.city === city)
+  $: res = res.filter(a => {
+    const now = new Date()
+    now.setHours(10,30)
+    const day = now.getDay()
+    const now_minutes = now.getHours() * 60 + now.getMinutes();
+    const [from, to] = day < 5 ? a.delovniCasTeden : day == 5 ? a.delovniCasSobota : a.delovniCasNedelja;
+    return !justOpen ? true : from < now_minutes && now_minutes < to;
+  })
+  
+</script>
+
+<div>
+  <h1>Boni</h1>
+  <div class="boni">
+    {#each res as restaurant}
+      <Restaurant data={restaurant} />
+    {/each}
+  </div>
+    
+</div>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+  .boni {
+    display: flex;
+    flex-direction:column;
+    justify-items:center;
+    align-items: center;
+  }
+</style>
---
+++ src/routes/Restaurant.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let data;
+  let directionsUrl = `https://www.google.com/maps/dir/?api=1&destination=${data.lat},${data.lon}`;
+</script>
+
+
+<div class="restaurant" href={data.detailslink}>
+  <a href={data.detailslink}><h2>{data.lokal}</h2></a>
+  <div class="doplacilo">
+    <a href={directionsUrl} target="_blank" rel="noreferrer">↱</a>
+    <div>{data.doplacilo} €</div>
+  </div>
+</div>
+
+<style>
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .doplacilo {
+    text-align: center;
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+
+  .restaurant {
+    width: 90vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #eeeeee;
+    border-radius: 10px;
+    padding-left: 10px;
+    padding-right: 20px;
+    margin-bottom: 10px;
+  }
+</style>
